06fc9e831d2162635c9140e73bc9f26fd9664d23
roiby <roiby92@gmail.com>
2020-09-29 18:55:18 +0300

    pushing at the end of the day

--- src/App.js
+++ src/App.js
@@ -3,19 +3,33 @@ import './App.css';
 
 class App extends Component {
   getStuff() {
+    const name = 'roi'
+    return (
+      <div>{name}</div>
+    )
   }
 
-  getMorningGreeting() {
-  }
+  getMorningGreeting = () => "good morning"
 
-  getEveningGreeting() {
-  }
+  getEveningGreeting = () => "good eavning"
 
-  showCompany(name, revenue) {
+  showCompany = (name, revenue) => {
+    return <div id={name}>{name} makes {revenue} every year</div>
   }
 
   getClassName(temperature) {
+     let name =null 
+    if (temperature < 15){
+       name = "freezing"
+    }
+    else if (temperature >=15 && temperature <30){
+      name = "fair" 
+    }
+    else{
+      name = "hell-scape"
+    }
 
+    return <div id="weatherBox" class={name}></div>
   }
 
   render() {
@@ -25,41 +39,48 @@ class App extends Component {
       { name: "Microsoft", revenue: 300 },
       { name: "Google", revenue: 600 }
     ]
+    const companiesWeaper = companies.map(c => {
+      return this.showCompany(c.name, c.revenue)
+    })
 
     return (
       <div>
         <div className="ex-space">
           <h4 className='ex-title'>Spot-check 1</h4 >
           <div className="exercise" id="spotcheck-1">
-            {/* your code here */}
+            {this.getStuff()}
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className='ex-title'>Spot-check 2</h4>
           <div className="exercise" id="spotcheck-2">
-            {/* your code here */}
+            {Date.now() < "12:00" ? this.getMorningGreeting() : this.getEveningGreeting()}
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className='ex-title'>Spot-check 3</h4>
           <div className="exercise" id="spotcheck-3">
-            {/* your code here */}
+            {[
+              this.getMorningGreeting(),
+              this.getEveningGreeting(),
+              <p>some text</p>
+            ]}
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className='ex-title'>Exercise 1</h4>
           <div className="exercise" id="ex-1">
-            {/* your code here */}
+            {companiesWeaper}
           </div>
         </div>
 
         <div className="ex-space">
           <h4 className='ex-title'>Exercise 2</h4>
           <div className="exercise" id="ex-2">
-            {/* your code here */}
+            {this.getClassName(15)}
           </div>
         </div>
       </div>
